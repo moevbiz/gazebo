5482a2783a6e6194c1a65697b7b215ebe4e7c200
Bruno Mokross <bruno.mokross@gmail.com>
2019-11-23 19:19:02 +0100

    router points to image folders

--- app.js
+++ app.js
@@ -1,7 +1,48 @@
 const express = require('express')
+const path = require('path')
+const fs = require('fs')
+
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000
+const dir =  process.cwd(); // cwd
 
-app.get('/', (req, res) => res.send('Hello Gazebo!'))
+//slug
+app.get ('/content/:slug', (req, res) => {
+    let slug = req.params.slug || '';
+    let imgFolder;
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+    if (slug) {
+        imgFolder = path.join(__dirname, req.originalUrl, '/images/')
+    };
+
+    var data = [];
+
+    browse(imgFolder, 'images');
+
+    function browse(url, arrayName) {
+        fs.readdir(url, (err, files) => {
+            console.log("browsing", url);
+            if (err) throw err
+            files.forEach(file => {
+                try {
+                    data.push({
+                        'name': file,
+                        'path': path.join(url, file)
+                    })
+                }
+                catch(e) {
+                    console.log(e)
+                }
+            })
+            res.send(data)
+        })
+    }
+
+})
+
+// static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`)
+})
