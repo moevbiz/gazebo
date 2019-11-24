a3da6e948e4f9282abbeb4d68a2dc0fa1a722c79
Bruno Mokross <bruno.mokross@gmail.com>
2019-11-24 18:23:39 +0100

    pug views

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express()
 const PORT = process.env.PORT || 3000
 const dir =  process.cwd(); // cwd
 
+app.set('view engine', 'pug')
+
 const content = path.join(__dirname, '/content')
 const gazebos = []
 
@@ -21,40 +23,82 @@ fs.readdir(content, (err, files) => {
     })
 })
 
-//slug
-app.get ('/content/:slug', (req, res) => {
-    let slug = req.params.slug || '';
-
-    if (slug && gazebos.includes(slug)) {
-        let imgFolder = path.join(__dirname, req.originalUrl, '/images/')
-        let data = [];
-        browse(imgFolder, 'images', data)
-    } else {
-        res.send('nothing here')
-    };
-
-    function browse(url, arrayName, data) {
-        let thisData = [];
-        fs.readdir(url, (err, files) => {
-            if (err) throw err
-            files.forEach(file => {
-                try {
-                    thisData.push({
-                        'name': file,
-                        'path': path.join(url, file),
-                        'ext': path.extname(file),
-                    })
-                }
-                catch(e) {
-                    console.log(e)
-                }
-            })
-            data.push({[arrayName]: thisData})
-            res.send(data);
+function render(thisGazebo, req, res) {
+    let images = []
+    let imgFolder = path.join(__dirname, 'content/', thisGazebo, '/images/')
+    fs.readdir(imgFolder, (err, files) => {
+        let data = []
+        if (err) throw err
+        files.forEach(file => {
+            try {
+                data.push({
+                    'name': file,
+                    'path': path.join(imgFolder, file),
+                    'ext': path.extname(file),
+                })
+            }
+            catch(e) {
+                console.log(e)
+            }
         })
-    }
+        res.render('view', {
+            title: thisGazebo,
+            gazebos: gazebos,
+            images: data
+        })
+    })
+}
 
+//homepage
+app.get('/', (req, res) => {
+    let gzb = gazebos[gazebos.length -1]
+    render(gzb, req, res)
 })
+
+app.get('/:slug', (req, res) => {
+    let slug = req.params.slug;
+    if (gazebos.includes(slug)) {
+        render (slug, req, res)
+    }
+    else {
+        res.render('error');
+    }
+})
+
+//slug
+// app.get ('/:slug', (req, res) => {
+//     let slug = req.params.slug || '';
+
+//     if (slug && gazebos.includes(slug)) {
+//         let imgFolder = path.join(__dirname, 'content/', req.originalUrl, '/images/')
+//         let data = [];
+//         browse(imgFolder, 'images', data)
+//     } else {
+//         res.send('nope')
+//     };
+
+//     function browse(url, arrayName, data) {
+//         let thisData = [];
+//         fs.readdir(url, (err, files) => {
+//             if (err) throw err
+//             files.forEach(file => {
+//                 try {
+//                     thisData.push({
+//                         'name': file,
+//                         'path': path.join(url, file),
+//                         'ext': path.extname(file),
+//                     })
+//                 }
+//                 catch(e) {
+//                     console.log(e)
+//                 }
+//             })
+//             data.push({[arrayName]: thisData})
+//             res.send(data);
+//         })
+//     }
+
+// })
 
 // static folder
 app.use(express.static(path.join(__dirname, 'public')));
