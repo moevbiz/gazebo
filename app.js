4cd95d87749cf6397521c8956a243b71eb373fc7
Bruno Mokross <bruno.mokross@gmail.com>
2020-02-17 12:32:28 +0100

    🏛

--- app.js
+++ app.js
@@ -23,12 +23,64 @@ fs.readdir(content, (err, files) => {
     })
 })
 
+//browse directory and subdirectories
+function browse(currentPath) {
+    let files = fs.readdirSync(currentPath)
+    files.forEach(file => {
+        try {
+            var stats = fs.statSync(path.join(currentPath, file))
+            if (stats.isFile()) {
+                console.log(file)
+            }
+            else if (stats.isDirectory()) {
+                newPath = path.join(currentPath, file)
+                browse(newPath);
+            }
+        }
+        catch(e) {
+            console.log(e)
+        }
+    })
+}
+
+// render page
 function render(thisGazebo, req, res) {
-    let images = []
-    let imgFolder = path.join(__dirname, 'content/', thisGazebo, '/images/')
-    fs.readdir(imgFolder, (err, files) => {
-        let images = []
+    let imageFolder = path.join(__dirname, 'content/', thisGazebo, '/images/')
+    let dir = path.join(__dirname, 'content/', thisGazebo)
+    fs.readdir(imageFolder, (err, files) => {
         if (err) throw err
+        let images = []
+        files.forEach(file => {
+            try {
+                images.push({
+                    'name': file,
+                    'path': path.join('content/', thisGazebo, '/images', file),
+                    'ext': path.extname(file),
+                })
+            }
+            catch(e) {
+                console.log(e)
+            }
+        })
+        res.render('view', {
+            title: thisGazebo,
+            gazebos: gazebos,
+            images: images
+        })
+    })
+}
+
+// render page
+function render2(thisGazebo, req, res) {
+    let imageFolder = path.join(__dirname, 'content/', thisGazebo, '/images/')
+    let dir = path.join(__dirname, 'content/', thisGazebo)
+    let files = fs.readdirSync(dir)
+    files.forEach(file => {
+
+    })
+    fs.readdir(dir, (err, files) => {
+        if (err) throw err
+        let images = []
         files.forEach(file => {
             try {
                 images.push({
@@ -52,7 +104,9 @@ function render(thisGazebo, req, res) {
 //homepage
 app.get('/', (req, res) => {
     let gzb = gazebos[gazebos.length -1]
+    // let dir = path.join(__dirname, 'content/', gzb)
     render(gzb, req, res)
+    // browse(dir)
 })
 
 //slug
@@ -62,7 +116,7 @@ app.get('/:slug', (req, res) => {
         render (slug, req, res)
     }
     else {
-        res.render('error');
+        res.redirect('/');
     }
 })
 
